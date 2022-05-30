0a4effeb7b1bc5661f2560dce074a96fb8565828
INA <codechemi@gmail.com>
2022-05-30 15:14:50 +0900

    Add : REST API Template.

--- App/Controllers/ApiController.cs
+++ App/Controllers/ApiController.cs
@@ -18,6 +18,11 @@ namespace ALIMS.Controllers
         }
 
         #region 사용자
+        /// <summary>
+        /// 사용자 정보
+        /// </summary>
+        /// <param name="UserEmail">사용자 이메일</param>
+        /// <returns></returns>
         [HttpGet]
         public string GetUserInfo(string UserEmail)
         {
@@ -25,6 +30,10 @@ namespace ALIMS.Controllers
             return JsonSerialize;
         }
 
+        /// <summary>
+        /// 역할(권한)이 없는 사용자 조회
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public string GetIdentityNullRoleUsers()
         {
@@ -32,6 +41,10 @@ namespace ALIMS.Controllers
             return JsonSerialize;
         }
 
+        /// <summary>
+        /// 미등록 사용자 조회
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public string GetIdentityNullUsers()
         {
@@ -51,6 +64,20 @@ namespace ALIMS.Controllers
         }
 
         /// <summary>
+        /// 권한 수정
+        /// </summary>
+        /// <param name="RoleId">역할 ID</param>
+        /// <param name="RoleName">역할 명</param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult UpdateRole(string RoleId, string RoleName)
+        {
+            _AppBll.UpdateRole(RoleId, RoleName);
+
+            return Redirect("~/Admin/RolesList");
+        }
+
+        /// <summary>
         /// 사용자 제거
         /// </summary>
         /// <param name="UserId">사용자 ID</param>
@@ -63,7 +90,25 @@ namespace ALIMS.Controllers
 
             return Ok();
         }
-        #endregion
 
+        /// <summary>
+        /// 사용자 정보 수정
+        /// </summary>
+        /// <param name="UserCd"></param>
+        /// <param name="UserEmails"></param>
+        /// <param name="UserDept"></param>
+        /// <param name="UserContact"></param>
+        /// <param name="UserAuthorization"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult UpdateUserInfomation(string UserCd, string UserEmails, string UserDept, string UserContact, string UserAuthorization)
+        {
+            _AppBll.UpdateUser(UserCd, UserDept, UserContact);
+            _AppBll.GrantAuthorizationUser(UserEmails, UserAuthorization);
+
+            return Redirect("~/Admin/Index");
+        }
+
+        #endregion
     }
 }
--- App/wwwroot/js/site.js
+++ App/wwwroot/js/site.js
@@ -13,6 +13,11 @@ function locationProfilePage() {
     location.href = defaultURL + "Identity/Account/Manage";
 }
 
+function locationAPIsPage() {
+    location.href = defaultURL + "swagger/";
+}
+
+
 function locationUnAuthorizationPage() {
     location.href = defaultURL + "Admin/UnAuthorizationList";
 }
@@ -31,4 +36,82 @@ function locationRolesListPage() {
 
 function locationLockListPage() {
     location.href = defaultURL + "Admin/LockList";
+}
+
+// Request APIs
+const APIList = [{
+    "GET": [{ "": "" }],
+    "POST": [{ "": "" }],
+    "PUT": [
+        { "Update User Infomation": "UpdateUserInfomation?" },
+        { "Update User Infomation": "UpdateRole?" },
+    ],
+    "DELETE": [
+        { "Remove User": "RemoveUser?" },
+        { "Delete Role": "DeleteRole?" }    ]
+    }
+]
+
+
+// Index View.
+let RequestUpdateUserInfomation = () => {
+    let RequestApiURL = "";
+    let APIArgument = "UserCd=" + $('#UserCd').val();
+    APIArgument += "&UserEmails=" + $('#UserEmails').val();
+    APIArgument += "&UserDept=" + $('#UserDept').val();
+    APIArgument += "&UserContact=" + $('#UserContact').val();
+    APIArgument += "&UserAuthorization=" + $('#AuthorizationName').val();
+
+    RequestApiURL += defaultRequesteURL + APIList["PUT"]["Update User Infomation"] + APIArgument
+
+    console.log(RequestApiURL)
+    requestAJAX(RequestApiURL,"PUT");
+}
+
+let RequestDeleteUser = () => {
+    let RequestApiURL = "";
+    let APIArgument = "RemoveUserCd=" + $('#RemoveUserCd').val();
+    APIArgument += "&RemoveUserId=" + $('#RemoveUserId').val();
+
+    RequestApiURL += defaultRequesteURL + APIList["DELETE"]["Remove User"] + APIArgument
+
+    console.log(RequestApiURL)
+    requestAJAX(RequestApiURL, "DELETE");
+}
+
+// Roles List View
+let RequestUpdateRole = () => {
+    let RequestApiURL = "";
+    let APIArgument = "UserCd=" + $('#UserCd').val();
+    APIArgument += "&UserDept=" + $('#UserEmails').val();
+    APIArgument += "&UserContact=" + $('#UserDept').val();
+    APIArgument += "&UserContact=" + $('#UserContact').val();
+    APIArgument += "&UserAuthorization=" + $('#UserAuthorization').val();
+
+    RequestApiURL += defaultRequesteURL + APIList["PUT"]["Update Role"] + APIArgument
+
+    console.log(RequestApiURL)
+    requestAJAX(RequestApiURL, "PUT");
+}
+
+let RequestDeleteRole = () => {
+    let RequestApiURL = "";
+    let APIArgument = "RemoveRoleId=" + $('#RemoveRoleId').val();
+
+    RequestApiURL += defaultRequesteURL + APIList["DELETE"]["Remove Role"] + APIArgument
+
+    console.log(RequestApiURL)
+    requestAJAX(RequestApiURL, "DELETE");
+}
+
+
+function requestAJAX(URL,Method) {
+    $.ajax({
+        url: URL,
+        method: Method,
+        dataType: "text",
+        success: function (data) {
+            console.log(data);
+        }
+    })
 }
