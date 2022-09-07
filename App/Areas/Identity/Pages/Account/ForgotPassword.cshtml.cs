0c18bc3d46c1924d0e57cadf753d1cb103509185
INA <codechemi@gmail.com>
2022-09-07 13:49:59 +0900

    Add Template Email Form

--- App/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ App/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
-using System.Text.Encodings.Web;
 
 namespace App.Areas.Identity.Pages.Account
 {
@@ -54,33 +53,9 @@ namespace App.Areas.Identity.Pages.Account
                     values: new { area = "Identity", code },
                     protocol: Request.Scheme);
 
+                string HtmlString = System.IO.File.ReadAllText(Path.Combine(callbackUrl, "/Account/ForgotPassword.html"));
                 await _emailSender.SendEmailAsync(
-                    Input.Email,
-                    "[ASP Net Core] 비밀번호 재설정",
-                    $"<!DOCTYPE html>" +
-                    $"<head>" +
-                    $"    <meta charset='utf-8' />" +
-                    $"    <meta name='viewport' content='width=device-width, initial-scale=1.0' />" +
-                    $"    <link rel='stylesheet' href='{callbackUrl.Split("/")[0]}//{callbackUrl.Split("/")[1]}/{callbackUrl.Split("/")[2]}/lib/bootstrap/dist/css/bootstrap.min.css' />" +
-                    $"</head>" +
-                    $"<body>" +
-                    $"    <div class='bg-light border rounded-3' style='margin:15px;'>" +
-                    $"        <div class='card text-center'>" +
-                    $"            <div class='card-body'>" +
-                    $"              <h3 class='card-title'>" +
-                    $"                <b>ASP Net Core</b>" +
-                    $"              </h3>" +
-                    $"              <p class='card-text'>" +
-                    $"                  비밀번호를 재설정 하시려면<br>아래 버튼을 눌러주세요." +
-                    $"              </p>" +
-                    $"              <a href='{HtmlEncoder.Default.Encode(callbackUrl)}' class='btn btn-primary'>비밀번호 재설정</a>" +
-                    $"            </div>" +
-                    $"        </div>" +
-                    $"    </div>" +
-                    $"    <script src='{callbackUrl.Split("/")[0]}//{callbackUrl.Split("/")[1]}/{callbackUrl.Split("/")[2]}/wwwroot/lib/jquery/dist/jquery.min.js'></script>" +
-                    $"    <script src='{callbackUrl.Split("/")[0]}//{callbackUrl.Split("/")[1]}/{callbackUrl.Split("/")[2]}/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.min.js'></script>" +
-                    $"</body>" +
-                    $"</html>");
+                    Input.Email, "Forgot Password", HtmlString);
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
             }
